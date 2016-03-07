5f866099cfec5701fe02f9aedd8ba3d3025ca12e
黄少华 <651024752@qq.com>
2016-03-07 11:37:39 +0800

    Initial Commit

---
+++ textBorder/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  textBorder
+//
+//  Created by 黄少华 on 16/3/7.
+//  Copyright © 2016年 黄少华. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ textBorder/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  textBorder
+//
+//  Created by 黄少华 on 16/3/7.
+//  Copyright © 2016年 黄少华. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
